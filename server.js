b6982e36f08ef71c6557475652f4c1d4c57f09ab
mlrodrigues <42525727+mlrodrigues@users.noreply.github.com>
2022-06-22 16:57:48 -0300

    Todas as informaçoes dos laptops

--- server.js
+++ server.js
@@ -11,19 +11,20 @@ app.get('/', async (req, res) => {
   await page.goto(url);
   
   const pageData = await page.evaluate(() => {
-    let data = [];
-		let elements = document.querySelectorAll('.thumbnail');
+    const data = [];
+		const elements = document.querySelectorAll('.thumbnail');
 
     for(var element of elements){
-			let textSplit = element.childNodes[3].innerText.split("\n");
-			let price = textSplit[0];
-			let title = textSplit[1];
-			let description = textSplit[3].replace('\"', '"');
-			let image = element.childNodes[1].src;
+			const textSplit = element.childNodes[3].innerText.split("\n");
+			const price = textSplit[0];
+			const title = textSplit[1];
+			const description = textSplit[3].replace('\"', '"');
+			const image = element.childNodes[1].src;
+      const ratings = element.childNodes[5].innerText.replace('\n\n','');
+      const startnumber = element.childNodes[5].childNodes[3].childElementCount
 
 			if(title.indexOf("Lenovo") != -1){
-				data.push({image, price, title, description});
-				console.log(data);
+				data.push({image, price, title, description, ratings, startnumber});
 			}
 		}
 
@@ -32,17 +33,19 @@ app.get('/', async (req, res) => {
 
   fs.writeFile('laptops.json', JSON.stringify(pageData, null, 2), err =>{
 		if(err) throw new Error('Something went wrong');
-		console.log('well done')
 	});
 
   await browser.close();
   console.log('Bot finalizado!');
 
-  res.send( 
+  res.send(
     pageData.map(element => ({
       image: element.image,
       title: element.title,
-      description: element.description
+      price: element.price,
+      description: element.description,
+      ratings: element.ratings,
+      startnumber: element.startnumber
     }))
   )
 });
