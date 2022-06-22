bde6184570a4dda25fd5edbec734137c9e57a9e7
mlrodrigues <42525727+mlrodrigues@users.noreply.github.com>
2022-06-22 17:56:50 -0300

    comentarios

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 
+//URL da pagina a ser buscada;
 const url = 'https://webscraper.io/test-sites/e-commerce/allinone/computers/laptops';
 
 app.get('/', async (req, res) => {
@@ -14,6 +15,7 @@ app.get('/', async (req, res) => {
     const data = [];
 		const elements = document.querySelectorAll('.thumbnail');
 
+    //Separar os dados que vem dos elements
     for(var element of elements){
 			const textSplit = element.childNodes[3].innerText.split("\n");
 			const price = textSplit[0];
@@ -23,7 +25,8 @@ app.get('/', async (req, res) => {
       const ratings = element.childNodes[5].innerText.replace('\n\n','');
       const startnumber = element.childNodes[5].childNodes[3].childElementCount
 
-			if(title.indexOf("Lenovo") != -1){
+			//Mandar os dados somente dos laptops Lenovo para o array 
+      if(title.indexOf("Lenovo") != -1){
 				data.push({image, price, title, description, ratings, startnumber});
 			}
 		}
@@ -31,6 +34,7 @@ app.get('/', async (req, res) => {
     return data;
   });
 
+  //Mandar os dados coletados para um json
   fs.writeFile('laptops.json', JSON.stringify(pageData, null, 2), err =>{
 		if(err) throw new Error('Something went wrong');
 	});
@@ -38,6 +42,7 @@ app.get('/', async (req, res) => {
   await browser.close();
   console.log('Bot finalizado!');
 
+  //Mostrar dados na tela; 
   res.send(
     pageData.map(element => ({
       image: element.image,
